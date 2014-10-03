2cc404e6123b8432fe277c72dad029afab2b97a8
Mark Westerlund <mark.westerlund.dev@gmail.com>
2014-10-03 00:51:23 -0400

    Added Friends add/remove functionality Added Friends to list

---
+++ src/com/markwesterlund/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.markwesterlund.ribbit;
+
+public final class ParseConstants {
+	
+	//Class Name
+	
+	
+	// Field Names
+	public static final String KEY_USERNAMES = "username";
+	public static final String KEY_FRIENDS_RELATION = "freindsRelation";
+
+}
