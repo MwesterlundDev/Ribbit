45b4119b16e199e7ecc78c0b7c2b4b27e457e791
Mark Westerlund <mark.westerlund.dev@gmail.com>
2014-10-02 21:51:38 -0400

    Attempting push

--- src/com/markwesterlund/ribbit/SignUpActivity.java
+++ src/com/markwesterlund/ribbit/SignUpActivity.java
@@ -45,6 +45,9 @@ public class SignUpActivity extends Activity {
 				password = password.trim();
 				email = email.trim();
 				
+				
+				//Mark is cool
+				
 				if(username.isEmpty() || password.isEmpty() || email.isEmpty()){
 					AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
 					builder.setMessage(R.string.signup_error_message)
