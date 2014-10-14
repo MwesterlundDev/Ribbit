289e30fb7df73f6aa56c68a222775581ea9b6b42
Mark Westerlund <mark.westerlund.dev@gmail.com>
2014-10-13 21:51:41 -0400

    Completed Themes for the app - added the background image/theme for logins - added the theme for action bar on each Activity

--- src/com/markwesterlund/ribbit/SignUpActivity.java
+++ src/com/markwesterlund/ribbit/SignUpActivity.java
@@ -20,6 +20,7 @@ public class SignUpActivity extends Activity {
 	protected EditText mPassword;
 	protected EditText mEmail;
 	protected Button mSignUpButton;
+	protected Button mCancelButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,18 @@ public class SignUpActivity extends Activity {
 		mPassword = (EditText) findViewById(R.id.passwordField);
 		mEmail = (EditText) findViewById(R.id.emailField);
 		mSignUpButton = (Button)findViewById(R.id.signUpButton);
+		
+		mCancelButton =  (Button)findViewById(R.id.cancelButton);
+		
+		mCancelButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
+		
 		
 		mSignUpButton.setOnClickListener(new View.OnClickListener() {
 			
@@ -98,6 +111,7 @@ public class SignUpActivity extends Activity {
 				
 			}
 		});
+		
 		
 	}
 
